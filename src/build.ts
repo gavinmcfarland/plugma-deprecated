21dd5a1f8ebe0e843caec0252e2d1aea20017064
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2021-03-22 20:36:53 +0000

    fixed: path issue

--- src/build.ts
+++ src/build.ts
@@ -2,45 +2,40 @@
  * This scripts appends the current version number to every node created.
  */
 
+// TODO: Is it too hard to try and add this after it's been minimised? I guess would run into this issue anyway, if someone used abbreviated form of declaring variables.
+
+const fs = require('fs')
+
 export default function build() {
 	console.log("hello")
+
+	var path
+
+	if (process.env.NODE_ENV === 'test') {
+		path = process.cwd() + '/src/stub/'
+	} else {
+		path = process.cwd()
+	}
+
+	const pkg = require(path + "/package.json")
+
+	var list = [
+		'figma\.createFrame\(\)',
+		'figma\.createRectangle\(\)',
+		'figma\.createElipse\(\)',
+		'figma\.createPolygon\(\)',
+		'figma\.createStar\(\)',
+		'figma\.createLine\(\)'
+	]
+
+
+	fs.readFile(path + "/code.js", "utf8", (err, data) => {
+		data = data.replace(/((\w+)\s*=\s*figma\.createRectangle\(\)\s*[;|\n|,])/gmi, `$1;\n$2.setPluginData("version", ${JSON.stringify(pkg.version)});`)
+
+		fs.writeFile(path + "/code.js", data, (err) => {
+			if (err) throw err;
+			console.log('The file has been saved!');
+		});
+	})
 }
 
-
-// const fs = require('fs')
-
-// var path
-
-// if (process.env.NODE_ENV === 'test') {
-// 	path = process.cwd() + 'src/stub/'
-// } else {
-// 	path = process.cwd()
-// }
-
-// console.log(path)
-
-// const pkg = require(path + "/package.json")
-
-// var list = [
-// 	'figma\.createFrame\(\)',
-// 	'figma\.createRectangle\(\)',
-// 	'figma\.createElipse\(\)',
-// 	'figma\.createPolygon\(\)',
-// 	'figma\.createStar\(\)',
-// 	'figma\.createLine\(\)'
-// ]
-
-
-
-
-// fs.readFile(path + "/code.js", "utf8", (err, data) => {
-// 	data = data.replace(/((?:var|const|let) (\w+)\s*=\s*figma\.createRectangle\(\)\s*[;|\n])/gmi, `$1\n$2.setPluginData("version", ${JSON.stringify(pkg.version)})`)
-
-// 	fs.writeFile('code.js', data, (err) => {
-// 		if (err) throw err;
-// 		console.log('The file has been saved!');
-// 	});
-// })
-
-// const codeFile = require("./code.js").toString()
-// console.log(codeFile)
