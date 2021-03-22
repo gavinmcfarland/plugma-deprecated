5249c5ee9d409f3721f4f2a9806ae053c4da193f
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2021-03-22 22:44:40 +0000

    fixed: build now works on abbreviated variables

--- src/build.ts
+++ src/build.ts
@@ -30,7 +30,22 @@ export default function build() {
 
 
 	fs.readFile(path + "/code.js", "utf8", (err, data) => {
-		data = data.replace(/((\w+)\s*=\s*figma\.createRectangle\(\)\s*[;|\n|,])/gmi, `$1;\n$2.setPluginData("version", ${JSON.stringify(pkg.version)});`)
+		// 1. First search for variable with match.
+		// 2. Then search for matches within.
+		data = data.replace(/((?:const|var|let)\s*\w+\s*=\s*figma\.create\w+\D+(?:;|\n))/gmi, (match, p1, p2, p3, offset, string) => {
+
+			var matches = []
+			match.replace(/(\w+)\s*=\s*figma\./gmi, (match, p1, p2, p3, offset, string) => {
+				matches.push(p1)
+			})
+
+			matches = matches.map((item) => `${item}.setPluginData("version", ${JSON.stringify(pkg.version)})`)
+
+			var newString = matches.join(";") + ";"
+
+			return match + newString
+
+		})
 
 		fs.writeFile(path + "/code.js", data, (err) => {
 			if (err) throw err;
