ca5702d9e1d279f444875c8df7f10e7067714cc1
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2021-03-27 18:57:11 +0000

    updated: package.json

--- examples/webpack/code.ts
+++ examples/webpack/code.ts
@@ -1,5 +1,40 @@
-var rect = figma.createRectangle()
+import plugma from 'plugma'
 
-console.log(rect)
+plugma((plugin) => {
+	console.log(plugin)
+
+	plugin.ui = {
+		html: __html__,
+		width: 250,
+		height: 300
+	}
+
+	plugin.command('createRectangles', ({ ui, data }) => {
+
+		ui.show(data)
+
+		plugin.on('create-rectangles', (msg) => {
+
+			const nodes: SceneNode[] = [];
+			for (let i = 0; i < msg.count; i++) {
+				const rect = figma.createRectangle();
+				const rect2 = figma.createRectangle()
+				console.log(rect2);
+				rect.x = i * 150;
+				rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+				figma.currentPage.appendChild(rect);
+				nodes.push(rect);
+			}
+			figma.currentPage.selection = nodes;
+			figma.viewport.scrollAndZoomIntoView(nodes);
+			figma.closePlugin();
+		})
+
+		plugin.on('cancel', () => {
+			figma.closePlugin();
+		})
+	})
+
+})
 
 
